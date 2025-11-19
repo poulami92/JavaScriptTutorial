11474c45a0698315da6fe64d3e2d28f15bcf3fb7
PD <poulami92datta@gmail.com>
2025-11-19 18:42:55 +0530

    arrrow functions and objects

--- arrowFunction.js
+++ arrowFunction.js
@@ -9,5 +9,48 @@ const msg = ()=>
     console.log("hello")
 }
 msg()
-///
+
+//Accessing object fields with arrow expression func
+
+const person = {firstName:"PD",lastName:"Datta"}
+
+const fullName= (p)=>{ 
+    console.log(p.firstName+" "+p.lastName)
+}
+fullName(person)
+
+//pasing default values to function parameter
+const greet=(username='guest',age=2)=>`Hello ${username}, you are ${age} years old`
+const g1=greet()
+console.log(g1)
+const g2= greet("PD",30)
+console.log(g2)
+
+//Find max no using Math func
+
+const maxVal= (a,b,c)=>
+{
+    return Math.max(a,b,c)
+}
+
+const maxValue = maxVal(10,30,50)
+console.log(maxValue)
+
+//rest parameter...variable act as an array variable
+const sum = (...numbers)=>{
+    return numbers.reduce(function(sum,curr){
+        return sum+curr
+    },0)
+}
+
+const total=sum(1,2,3,4,5)
+console.log(total)
+
+const browserInfo = (browser='chrome',...details)=>{
+    console.log('Browser:'+browser)
+    console.log('other deatils:'+details)
+}
+
+browserInfo()
+browserInfo('firefox','mozilla','headless')
 
---
+++ objectCreate.js
@@ -0,0 +1,49 @@
+//Object Literals
+
+const user ={
+    name:'John',
+    age:30,
+    country:'USA'
+}
+
+console.log(user.name)
+
+//Constructor Function
+
+function Car(brand,model,price)
+{
+    this.brand=brand,
+    this.model=model,
+    this.price=price
+}
+
+const c1=new Car('BMW','52d',10000)
+const c2=new Car('Audi','52d',10000)
+
+console.log(c1.brand)
+console.log(c2.brand)
+
+//Class style
+
+class Customer{
+
+    constructor(name,product)
+    {
+        this.name=name,
+        this.product=product
+    }
+
+    addToCart()
+    {
+        console.log(this.product+" added to cart")
+    }
+
+}
+
+const cust1= new Customer('PD','MacBook')
+console.log(cust1.name)
+console.log(cust1.product)
+cust1.addToCart()
+
+
+
